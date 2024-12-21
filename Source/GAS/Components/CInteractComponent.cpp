b29024747953a489f53c0933e436e5ad4ff2f8f7
뇌가 고기가 된 사람 <65766852+simeddk@users.noreply.github.com>
2024-12-21 23:12:27 +0900

    상호작용 물체에 위젯 띄우기
    `F` 키를 눌러 조의를 표하십시오.

--- Source/GAS/Components/CInteractComponent.cpp
+++ Source/GAS/Components/CInteractComponent.cpp
@@ -1,6 +1,7 @@
 #include "CInteractComponent.h"
 #include "DrawDebugHelpers.h"
 #include "Interfaces/CGameplayInterface.h"
+#include "UI/CWorldWidget.h"
 
 static TAutoConsoleVariable<bool> CVarDebugDrawInteraction(TEXT("SGA.InteractionDebugDraw"), false, TEXT("Enable Debug Lines for Interact Component."), ECVF_Cheat);
 
@@ -8,6 +9,9 @@ UCInteractComponent::UCInteractComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 
+	TraceRadius = 30.0f;
+	TraceDistance = 500.0f;
+	CollisionChannel = ECC_WorldDynamic;
 }
 
 void UCInteractComponent::BeginPlay()
@@ -20,14 +24,15 @@ void UCInteractComponent::TickComponent(float DeltaTime, ELevelTick TickType, FA
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+	FindNearestInteractable();
 }
 
-void UCInteractComponent::PrimaryInteract()
+void UCInteractComponent::FindNearestInteractable()
 {
 	bool bDebugDraw = CVarDebugDrawInteraction.GetValueOnGameThread();
 
 	FCollisionObjectQueryParams ObjectQueryParams;
-	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	ObjectQueryParams.AddObjectTypesToQuery(CollisionChannel);
 
 	AActor* MyOwner = GetOwner();
 
@@ -35,24 +40,25 @@ void UCInteractComponent::PrimaryInteract()
 	FRotator EyeRotation;
 	MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
 
-	FVector End = EyeLocation + (EyeRotation.Vector() * 1000);
+	FVector End = EyeLocation + (EyeRotation.Vector() * TraceDistance);
 
 	//SweepTrace
 	TArray<FHitResult> Hits;
 
-	float Radius = 30.f;
 	FCollisionShape Shape;
-	Shape.SetSphere(Radius);
+	Shape.SetSphere(TraceRadius);
 
 	bool bBlockingHit = GetWorld()->SweepMultiByObjectType(Hits, EyeLocation, End, FQuat::Identity, ObjectQueryParams, Shape);
 
 	FColor LineColor = bBlockingHit ? FColor::Green : FColor::Red;
 
+	FocusedActor = nullptr;
+
 	for (FHitResult Hit : Hits)
 	{
 		if (bDebugDraw)
 		{
-			DrawDebugSphere(GetWorld(), Hit.ImpactPoint, Radius, 32, LineColor, false, 2.0f);
+			DrawDebugSphere(GetWorld(), Hit.ImpactPoint, TraceRadius, 32, LineColor, false, 2.0f);
 		}
 
 		AActor* HitActor = Hit.GetActor();
@@ -60,11 +66,35 @@ void UCInteractComponent::PrimaryInteract()
 		{
 			if (HitActor->Implements<UCGameplayInterface>())
 			{
-				APawn* MyPawn = Cast<APawn>(MyOwner);
+				FocusedActor = HitActor;
 
-				ICGameplayInterface::Execute_Interact(HitActor, MyPawn);
 				break;
 			}
+		}
+	}
+
+	if (FocusedActor)
+	{
+		if (DefaultWidgetInstance == nullptr && ensure(DefaultWidgetClass))
+		{
+			DefaultWidgetInstance = CreateWidget<UCWorldWidget>(GetWorld(), DefaultWidgetClass);
+		}
+
+		if (DefaultWidgetInstance)
+		{
+			DefaultWidgetInstance->AttachedActor = FocusedActor;
+
+			if (!DefaultWidgetInstance->IsInViewport())
+			{
+				DefaultWidgetInstance->AddToViewport();
+			}
+		}
+	}
+	else
+	{
+		if (DefaultWidgetInstance)
+		{
+			DefaultWidgetInstance->RemoveFromParent();
 		}
 	}
 
@@ -72,4 +102,17 @@ void UCInteractComponent::PrimaryInteract()
 	{
 		DrawDebugLine(GetWorld(), EyeLocation, End, LineColor, false, 2.0f, 0, 2.0f);
 	}
+}
+
+void UCInteractComponent::PrimaryInteract()
+{
+	if (FocusedActor == nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, "There is no interactable.");
+		return;
+	}
+
+	APawn* MyPawn = Cast<APawn>(GetOwner());
+
+	ICGameplayInterface::Execute_Interact(FocusedActor, MyPawn);
 }
--- Source/GAS/Components/CInteractComponent.h
+++ Source/GAS/Components/CInteractComponent.h
@@ -4,6 +4,7 @@
 #include "Components/ActorComponent.h"
 #include "CInteractComponent.generated.h"
 
+class UCWorldWidget;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class GAS_API UCInteractComponent : public UActorComponent
@@ -19,6 +20,28 @@ protected:
 public:	
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+protected:
+	void FindNearestInteractable();
+
 public:
 	void PrimaryInteract();
+
+protected:
+	UPROPERTY()
+	AActor* FocusedActor;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Trace")
+	float TraceDistance;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Trace")
+	float TraceRadius;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Trace")
+	TEnumAsByte<ECollisionChannel> CollisionChannel;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<UCWorldWidget> DefaultWidgetClass;
+
+	UPROPERTY()
+	UCWorldWidget* DefaultWidgetInstance;
 };
